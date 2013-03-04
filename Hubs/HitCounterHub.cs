d2f008f3df9f401c6c8abaf8381210588aeff183
dtomyn <dtomyn@hotmail.com>
2013-03-04 06:31:08 -0600

    started creation of SignalR demo

---
+++ Hubs/HitCounterHub.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebApplication1.Hubs
+{
+    [HubName("hitCounter")]
+    public class HitcounterHub : Hub
+    {
+        private static int _hitCount;
+        public void RecordHit()
+        {
+            _hitCount += 1;
+            //note... this is a dynamic method... think of the method as an event
+            Clients.All.updateHitCount(_hitCount);
+            //when this event fires on the server there is going to be a JavaScript event that will fire
+            //"All" means All callers will see the response
+            // also get "Caller"... only for the specific caller not everyone sees it
+            // also get "Groups"... only a private connection
+        }
+
+        public override Task OnDisconnected()
+        {
+            /* when you close the web browser then THIS will call it!!!! wow!! */
+            _hitCount -= 1;
+            Clients.All.updateHitCount(_hitCount);
+            return base.OnDisconnected();
+            //WOW!!!!!
+        }
+
+        /* the "hello world" if SignalR apps is Chat */
+
+    }
+}
