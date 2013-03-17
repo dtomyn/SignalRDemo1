e4ca639a8741bfe130961ffaca7c35dd7191d497
dtomyn <dtomyn@hotmail.com>
2013-03-16 18:01:57 -0600

    made to work better in older IE. added comments to code

--- Hubs/HitCounterHub.cs
+++ Hubs/HitCounterHub.cs
@@ -4,27 +4,37 @@ using System.Threading.Tasks;
 
 namespace WebApplication1
 {
-    [HubName("hitCounter")]
-    public class HitcounterHub : Hub
-    {
-        private static int _hitCount;
-        public void RecordHit()
-        {
-            _hitCount += 1;
-            //note... this is a dynamic method... think of the method as an event
-            Clients.All.updateHitCount(_hitCount);
-            //when this event fires on the server there is going to be a JavaScript event that will fire
-            //"All" means All callers will see the response
-            // also get "Caller"... only for the specific caller not everyone sees it
-            // also get "Groups"... only a private connection
-        }
+//attribute sets what "serviceProxy" that can create
+//default is class name
+[HubName("hitCounter")]
+public class HitcounterHub : Hub
+{
+    //holds the number of connected browsers
+    private static int _hitCount;
 
-        public override Task OnDisconnected()
-        {
-            /* when you close the web browser then THIS will call it!!!! wow!! */
-            _hitCount -= 1;
-            Clients.All.updateHitCount(_hitCount);
-            return base.OnDisconnected();
-        }
+    //server method that can be invoked by JavaScript
+    public void RecordHit()
+    {
+        _hitCount += 1;
+        //note... this is a dynamic method... think of the method as an event
+        Clients.All.updateHitCount(_hitCount);
+        //when this event fires on the server there is going to be a JavaScript 
+        //event that will fire
+        //NOTE:
+        //- "All" means All callers will see the response
+        //there are other more secure options here as well
     }
+
+    //SignalR will communicate automatically when the browser is disconnected
+    //For example, if close the browser window / tab, this will automatically
+    //be called!
+    public override Task OnDisconnected()
+    {
+        //decrease the counter
+        _hitCount -= 1;
+        //send a message back to all of the connected clients
+        Clients.All.updateHitCount(_hitCount);
+        return base.OnDisconnected();
+    }
+}
 }
