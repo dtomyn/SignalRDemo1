bbe0fb167322b6123e4352aa2f650a592658220f
Darek <dtomyn@hotmail.com>
2013-03-18 10:46:30 -0600

    another half baked signalr demo drawingboard
    courtesy of white paper received today

---
+++ Hubs/DrawingBoard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class DrawingBoard : Hub
+    {
+        public Task BroadcastPoint(float x, float y)
+        {
+            return Clients.Others.drawPoint(x, y, Clients.Caller.color);
+        }
+        public Task BroadcastClear()
+        {
+            return Clients.Others.clear();
+        }
+    }
+
+}
